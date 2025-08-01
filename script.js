218fb1290135b6499fd72b32af2b3d1749120d64
Fred <lance.seven100and77@gmail.com>
2025-08-01 16:57:21 +0100

    second commit

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-var button = document.getElementById('butt_for');
-button.onclick = function () {
-    var container = document.getElementById('boddy');
-    sideScroll(container,'right',50,600,30);
-};
+// document.addEventListener('DOMContentLoaded', function () {
+// var button = document.getElementById('butt_for');
+// button.onclick = function () {
+//     var container = document.getElementById('boddy');
+//     sideScroll(container,'right',50,600,30);
+// };
 
-var back = document.getElementById('butt_back');
-back.onclick = function () {
-    var container = document.getElementById('boddy');
-    sideScroll(container,'left',50,600,30);
-};
-});
+// var back = document.getElementById('butt_back');
+// back.onclick = function () {
+//     var container = document.getElementById('boddy');
+//     sideScroll(container,'left',50,600,30);
+// };
+// });
 
-function sideScroll(element,direction,speed,distance,step){
-    let scrollAmount = 0;
-    var slideTimer = setInterval(function(){
-        if(direction == 'left'){
-            element.scrollLeft -= step;
-        } else {
-            element.scrollLeft += step;
-        }
-        scrollAmount += step;
-        if(scrollAmount >= distance){
-            window.clearInterval(slideTimer);
-        }
-    }, speed);
-}
+// function sideScroll(element,direction,speed,distance,step){
+//     let scrollAmount = 0;
+//     var slideTimer = setInterval(function(){
+//         if(direction == 'left'){
+//             element.scrollLeft -= step;
+//         } else {
+//             element.scrollLeft += step;
+//         }
+//         scrollAmount += step;
+//         if(scrollAmount >= distance){
+//             window.clearInterval(slideTimer);
+//         }
+//     }, speed);
+// }
 
 
 
